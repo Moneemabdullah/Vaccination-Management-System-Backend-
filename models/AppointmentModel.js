a3f4de6188e7a9d28e8c302af4c43d3ad9991dca
moneem <moneem.all.abdullah@gmail.com>
2025-06-05 09:15:23 +0600

    vaccing, vaccination, vaccine, appointment, model, module

---
+++ models/AppointmentModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const appointmentSchema = new mongoose.Schema(
+    {
+        patient: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "PatientProfile",
+            required: true,
+        },
+        doctor: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "DoctorProfile",
+            required: true,
+        },
+        date: { type: Date, required: true },
+        time: { type: String, required: true },
+        status: {
+            type: String,
+            enum: ["Pending", "Confirmed", "Cancelled"],
+            default: "Pending",
+        },
+    },
+    { timestamps: true }
+);
+
+export const Appointment = mongoose.model("Appointment", appointmentSchema);
---
+++ models/VaccinationModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const vaccinationSchema = new mongoose.Schema({
+    patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "PatientProfile",
+        required: true,
+    },
+    vaccine: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Vaccine",
+        required: true,
+    },
+    doctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "DoctorProfile",
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ["Running", "Completed", "Cancelled"],
+        default: "Pending",
+    },
+});
+
+export const Vaccination = mongoose.model("Vaccination", vaccinationSchema);
